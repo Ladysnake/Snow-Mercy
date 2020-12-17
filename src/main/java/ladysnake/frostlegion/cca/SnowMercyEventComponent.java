75436d04107f9dee4582e2ea1e0bb34f6cb1ac44
doctor4t <25477005+doctor4t@users.noreply.github.com>
2020-12-17 15:28:11 +0100

    Add snowmercy command

---
+++ src/main/java/ladysnake/frostlegion/cca/SnowMercyEventComponent.java
@@ -0,0 +1,34 @@
+package ladysnake.frostlegion.cca;
+
+import dev.onyxstudios.cca.api.v3.component.sync.AutoSyncedComponent;
+import dev.onyxstudios.cca.api.v3.component.tick.ServerTickingComponent;
+import net.minecraft.nbt.CompoundTag;
+
+public class SnowMercyEventComponent implements BooleanComponent, AutoSyncedComponent, ServerTickingComponent {
+    private boolean isEventOngoing;
+
+    @Override
+    public boolean isEventOngoing() {
+        return this.isEventOngoing;
+    }
+
+    public void setEventOngoing(boolean bool) {
+        this.isEventOngoing = bool;
+    }
+
+    @Override
+    public void readFromNbt(CompoundTag compoundTag) {
+        this.isEventOngoing = compoundTag.getBoolean("SnowMercy");
+    }
+
+    @Override
+    public void writeToNbt(CompoundTag compoundTag) {
+        compoundTag.putBoolean("SnowMercy", this.isEventOngoing);
+    }
+
+    @Override
+    public void serverTick() {
+        System.out.println("WORKING");
+
+    }
+}
--- src/main/java/ladysnake/frostlegion/common/FrostLegion.java
+++ src/main/java/ladysnake/frostlegion/common/FrostLegion.java
@@ -1,9 +1,11 @@
 package ladysnake.frostlegion.common;
 
+import ladysnake.frostlegion.common.command.SnowMercyCommand;
 import ladysnake.frostlegion.common.init.Blocks;
 import ladysnake.frostlegion.common.init.EntityTypes;
 import ladysnake.frostlegion.common.init.Items;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 
 public class FrostLegion implements ModInitializer {
     public static final String MODID = "frostlegion";
@@ -13,5 +15,9 @@ public class FrostLegion implements ModInitializer {
         Blocks.init();
         Items.init();
         EntityTypes.init();
+
+        CommandRegistrationCallback.EVENT.register((commandDispatcher, b) ->
+                SnowMercyCommand.register(commandDispatcher)
+        );
     }
 }

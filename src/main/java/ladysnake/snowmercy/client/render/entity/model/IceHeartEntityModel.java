759bc7a7b972bc2de834a3f43d0f242994ee726e
doctor4t <25477005+doctor4t@users.noreply.github.com>
2021-12-21 19:58:57 +0100

    Make the heart of ice look unstable when active

--- src/main/java/ladysnake/snowmercy/client/render/entity/model/IceHeartEntityModel.java
+++ src/main/java/ladysnake/snowmercy/client/render/entity/model/IceHeartEntityModel.java
@@ -3,11 +3,14 @@ package ladysnake.snowmercy.client.render.entity.model;// Made with Blockbench 4
 // Paste this class into your mod and generate all required imports
 
 
+import ladysnake.snowmercy.common.entity.IceHeartEntity;
 import net.minecraft.client.model.*;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
+
+import java.util.Random;
 
 public class IceHeartEntityModel<T extends Entity> extends EntityModel<T> {
     private final ModelPart heart;
@@ -33,9 +36,15 @@ public class IceHeartEntityModel<T extends Entity> extends EntityModel<T> {
 
     @Override
     public void setAngles(T entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
-        this.heart.yaw = yaw += 0.001f;
-        this.heart.pitch = pitch += 0.001f;
-        this.heart.roll = roll += 0.001f;
+        if (entity instanceof IceHeartEntity && ((IceHeartEntity) entity).isActive()) {
+            this.heart.yaw = yaw += new Random().nextFloat()/20f;
+            this.heart.pitch = pitch += new Random().nextFloat()/20f;
+            this.heart.roll = roll += new Random().nextFloat()/20f;
+        } else {
+            this.heart.yaw = yaw += 0.001f;
+            this.heart.pitch = pitch += 0.001f;
+            this.heart.roll = roll += 0.001f;
+        }
 //        this.heart.setPivot(0f, (float) (89f+Math.sin(entity.age/50f)*10f), 0f);
     }
 

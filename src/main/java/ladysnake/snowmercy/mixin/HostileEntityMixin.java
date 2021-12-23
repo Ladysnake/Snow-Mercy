fb883f273495426042d39304161eb5d4f2a0250b
doctor4t <25477005+doctor4t@users.noreply.github.com>
2021-12-23 18:22:00 +0100

    Revert "Limit icepost generation to the murderland"
    This reverts commit 261e641bc1c40f0306629d698733f55c2adc3cb2.

---
+++ src/main/java/ladysnake/snowmercy/mixin/HostileEntityMixin.java
@@ -0,0 +1,30 @@
+package ladysnake.snowmercy.mixin;
+
+import ladysnake.snowmercy.common.SnowMercy;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(HostileEntity.class)
+public abstract class HostileEntityMixin extends LivingEntity {
+    protected HostileEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "canSpawnInDark", at = @At("RETURN"), cancellable = true)
+    private static void canSpawnInDark(EntityType<? extends HostileEntity> type, ServerWorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> callbackInfoReturnable) {
+        if (world.getDimension().getCoordinateScale() == 2 && !world.getDimension().isNatural() && !world.getDimension().isUltrawarm() && world.getDimension().isBedWorking() && world.getDimension().hasRaids()) {
+            callbackInfoReturnable.setReturnValue(false);
+        }
+    }
+}
